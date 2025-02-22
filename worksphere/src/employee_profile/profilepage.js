e9bdde364755490ff4b8d2161c117a0d1aca2d5b
Dhruvil1802 <dhruvilpatel180201@gmail.com>
2025-02-22 06:18:03 -0500

    integrated delete and get API in profile page

--- worksphere/src/employee_profile/profilepage.js
+++ worksphere/src/employee_profile/profilepage.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import "./profilepage.css";
 
 const EmployeeProfile = () => {
-  const [allDetails, setAllDetails] = useState([]);
+  // const [allDetails, setAllDetails] = useState([]);
   const [gereralDetails, setGeneralDetails] = useState([]);
   const [education, setEducation] = useState([]);
   const [skills, setSkills] = useState([]);
@@ -38,6 +38,185 @@ const EmployeeProfile = () => {
     }
     getAllDetails();
   }, []);
+
+  // delete education
+  async function deleteEducation(Id) {
+    const res = await fetch(
+      `http://127.0.0.1:8000/employeeprofile/employeeeducation/${Id}/`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await res.json();
+
+    if (data.status.code === 200) {
+      const res = await fetch(
+        `http://127.0.0.1:8000/employeeprofile/employeeeducation/`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await res.json();
+      setEducation(data.data);
+    }
+  }
+
+  // delete certification
+  async function deleteCertification(Id) {
+    const res = await fetch(
+      `http://127.0.0.1:8000/employeeprofile/employeecertifications/${Id}/`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await res.json();
+
+    if (data.status.code === 200) {
+      const res = await fetch(
+        `http://127.0.0.1:8000/employeeprofile/employeecertifications/`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await res.json();
+      setCertifications(data.data);
+    }
+  }
+
+  // delete skills
+  async function deleteSkills(Id) {
+    const res = await fetch(
+      `http://127.0.0.1:8000/employeeprofile/employeeskills/${Id}/`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await res.json();
+
+    if (data.status.code === 200) {
+      const res = await fetch(
+        `http://127.0.0.1:8000/employeeprofile/employeeskills/`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await res.json();
+      setSkills(data.data);
+    }
+  }
+
+  // delete achievements
+  async function deleteAchievements(Id) {
+    const res = await fetch(
+      `http://127.0.0.1:8000/employeeprofile/employeeachievements/${Id}/`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await res.json();
+
+    if (data.status.code === 200) {
+      const res = await fetch(
+        `http://127.0.0.1:8000//employeeprofile/employeeachievements/`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await res.json();
+      setAchievements(data.data);
+    }
+  }
+  // delete preference
+  async function deletePreference(Id) {
+    const res = await fetch(
+      `http://127.0.0.1:8000/employeeprofile/employeepreferences/${Id}/`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await res.json();
+
+    if (data.status.code === 200) {
+      const res = await fetch(
+        `http://127.0.0.1:8000/employeeprofile/employeepreferences/`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await res.json();
+      setPreferences(data.data);
+    }
+  }
+
+  // delete experience
+  async function deleteExperience(Id) {
+    const res = await fetch(
+      `http://127.0.0.1:8000/employeeprofile/employeeexperiences/${Id}/`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await res.json();
+
+    if (data.status.code === 200) {
+      const res = await fetch(
+        `http://127.0.0.1:8000/employeeprofile/employeeexperiences/`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await res.json();
+      setExperiences(data.data);
+    }
+  }
 
   return (
     <>
@@ -104,24 +283,15 @@ const EmployeeProfile = () => {
                   {education.map((e, index) => (
                     <span key={index} className="education-item">
                       {e.education}
+                      <button
+                        className="cross-button"
+                        onClick={() => deleteEducation(e.employee_education_id)}
+                      ></button>
                     </span>
                   ))}
                 </div>
               </div>
             </div>
-
-            {/* <div className="section">
-              <h3 className="section-title">Experience</h3>
-              <div className="sub-section">
-                <div className="experience-container">
-                  {experiences.map((e, index) => (
-                    <span key={index} className="experience-item">
-                      {e.experience_institute}
-                    </span>
-                  ))}
-                </div>
-              </div>
-            </div> */}
 
             <div className="section">
               <h3 className="section-title">Experience</h3>
@@ -153,6 +323,12 @@ const EmployeeProfile = () => {
                   {certifications.map((c, index) => (
                     <span key={index} className="certifications-item">
                       {c.certificates}
+                      <button
+                        className="cross-button"
+                        onClick={() =>
+                          deleteCertification(c.employee_certification_id)
+                        }
+                      ></button>
                     </span>
                   ))}
                 </div>
@@ -166,10 +342,15 @@ const EmployeeProfile = () => {
                   {preferences.map((p, index) => (
                     <span key={index} className="preferences-item">
                       {p.preferences}
+                      <button
+                        className="cross-button"
+                        onClick={() =>
+                          deletePreference(p.employee_preferences_id)
+                        }
+                      ></button>
                     </span>
                   ))}
                 </div>
-                {/* <button class="add-button">Add</button> */}
               </div>
             </div>
 
@@ -180,6 +361,10 @@ const EmployeeProfile = () => {
                   {skills.map((s, index) => (
                     <span key={index} className="skills-item">
                       {s.skills}
+                      <button
+                        className="cross-button"
+                        onClick={() => deleteSkills(s.employee_skills_id)}
+                      ></button>
                     </span>
                   ))}
                 </div>
@@ -193,6 +378,12 @@ const EmployeeProfile = () => {
                   {achievements.map((a, index) => (
                     <span key={index} className="achievements-item">
                       {a.achievements}
+                      <button
+                        className="cross-button"
+                        onClick={() =>
+                          deleteAchievements(a.employee_achievements_id)
+                        }
+                      ></button>
                     </span>
                   ))}
                 </div>
